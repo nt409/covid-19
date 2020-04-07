132aa5adec84a3750b499349f615511184657a16
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2020-04-07 20:03:42 +0100

    Fixed aligning plots when also normalising

--- dan_get_data.py
+++ dan_get_data.py
@@ -11,11 +11,38 @@ except Exception as e:
     print("Failed to use COVID19 Python API", e)
     USE_API = False
 
-COUNTRY_LIST_WORLDOMETER = ['australia', 'uk', 'us', 'italy', 'spain', 'germany', 'iran',
-                            'france', 'ireland', 'china', 'south-korea', 'switzerland', 'netherlands',
-                            'austria', 'belgium', 'norway', 'sweden', 'portugal', 'brazil', 'canada',
-                            'denmark', 'malaysia', 'poland', 'greece', 'indonesia', 'philippines',
-                            'china-hong-kong-sar', 'iraq', 'algeria', 'world']
+COUNTRY_LIST_WORLDOMETER = ['afghanistan', 'albania', 'algeria', 'andorra', 'angola', 'anguilla', 'antigua-and-barbuda',
+                            'argentina', 'armenia', 'aruba', 'australia', 'austria', 'azerbaijan', 'bahamas', 'bahrain',
+                            'bangladesh', 'barbados', 'belarus', 'belgium', 'belize', 'benin', 'bermuda', 'bhutan',
+                            'bolivia', 'bosnia-and-herzegovina', 'botswana', 'brazil', 'british-virgin-islands',
+                            'brunei-darussalam', 'bulgaria', 'burkina-faso', 'burundi', 'cabo-verde', 'cambodia',
+                            'cameroon', 'canada', 'caribbean-netherlands', 'cayman-islands', 'central-african-republic',
+                            'chad', 'channel-islands', 'chile', 'china-hong-kong-sar', 'china-macao-sar', 'china',
+                            'colombia', 'congo', 'costa-rica', 'cote-d-ivoire', 'croatia', 'cuba', 'curacao',
+                            'cyprus', 'czech-republic', 'democratic-republic-of-the-congo', 'denmark', 'djibouti',
+                            'dominica', 'dominican-republic', 'ecuador', 'egypt', 'el-salvador', 'equatorial-guinea',
+                            'eritrea', 'estonia', 'ethiopia', 'faeroe-islands', 'falkland-islands-malvinas', 'fiji',
+                            'finland', 'france', 'french-guiana', 'french-polynesia', 'gabon', 'gambia', 'georgia',
+                            'germany', 'ghana', 'gibraltar', 'greece', 'greenland', 'grenada', 'guadeloupe',
+                            'guatemala', 'guinea-bissau', 'guinea', 'guyana', 'haiti', 'holy-see', 'honduras',
+                            'hungary', 'iceland', 'india', 'indonesia', 'iran', 'iraq', 'ireland', 'isle-of-man',
+                            'israel', 'italy', 'jamaica', 'japan', 'jordan', 'kazakhstan', 'kenya', 'kuwait',
+                            'kyrgyzstan', 'laos', 'latvia', 'lebanon', 'liberia', 'libya', 'liechtenstein',
+                            'lithuania', 'luxembourg', 'macedonia', 'madagascar', 'malawi', 'malaysia', 'maldives',
+                            'mali', 'malta', 'martinique', 'mauritania', 'mauritius', 'mayotte', 'mexico', 'moldova',
+                            'monaco', 'mongolia', 'montenegro', 'montserrat', 'morocco', 'mozambique', 'myanmar',
+                            'namibia', 'nepal', 'netherlands', 'new-caledonia', 'new-zealand', 'nicaragua', 'niger',
+                            'nigeria', 'norway', 'oman', 'pakistan', 'panama', 'papua-new-guinea', 'paraguay', 'peru',
+                            'philippines', 'poland', 'portugal', 'qatar', 'reunion', 'romania', 'russia', 'rwanda',
+                            'saint-barthelemy', 'saint-kitts-and-nevis', 'saint-lucia', 'saint-martin',
+                            'saint-pierre-and-miquelon', 'saint-vincent-and-the-grenadines', 'san-marino',
+                            'sao-tome-and-principe', 'saudi-arabia', 'senegal', 'serbia', 'seychelles', 'sierra-leone',
+                            'singapore', 'sint-maarten', 'slovakia', 'slovenia', 'somalia', 'south-africa',
+                            'south-korea', 'south-sudan', 'spain', 'sri-lanka', 'state-of-palestine', 'sudan',
+                            'suriname', 'swaziland', 'sweden', 'switzerland', 'syria', 'taiwan', 'tanzania',
+                            'thailand', 'timor-leste', 'togo', 'trinidad-and-tobago', 'tunisia', 'turkey',
+                            'turks-and-caicos-islands', 'uganda', 'uk', 'ukraine', 'united-arab-emirates', 'uruguay',
+                            'us', 'uzbekistan', 'venezuela', 'viet-nam', 'western-sahara', 'zambia', 'zimbabwe']
 
 
 def get_data(country_name):
