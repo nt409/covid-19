f5b7218f56bdd929e7f8a56b88df7cad15f1a1b4
nt409 <nt409@cam.ac.uk>
2020-04-09 14:45:18 +0100

    tidying

--- app.py
+++ app.py
@@ -4864,7 +4864,6 @@ def update_plots(n_clicks, start_date, end_date, show_exponential, normalise_by_
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-    # app2.run_server(debug=True)
 
 
 
